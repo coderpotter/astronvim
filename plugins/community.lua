855a12ac20e36a3b37a3eb3a8002a90bcbc5ee98
coderpotter <anighojkar@usf.edu>
2023-09-02 09:07:44 -0400

    code runner and highlighting

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,8 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
+  { import = "astrocommunity.code-runner.compiler-nvim" },
+  { import = "astrocommunity.color.vim-highlighter" },
   { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
